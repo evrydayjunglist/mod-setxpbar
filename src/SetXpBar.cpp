146784209dec102882fbb3fc57c11158ac35c123
Brian Oost <brianleon77@gmail.com>
2025-05-01 00:48:50 +0200

    Remove verbose, it's useless, clutters console for no good info.

--- src/SetXpBar.cpp
+++ src/SetXpBar.cpp
@@ -11,8 +11,6 @@ std::vector<std::string> SetXpBarServerScript::GetChunks(std::string s, uint8_t 
 
 void SetXpBarServerScript::SendChunkedPayload(Warden* warden, WardenPayloadMgr* payloadMgr, std::string payload, uint32 chunkSize)
 {
-    bool verbose = sConfigMgr->GetOption<bool>("SetXpBar.Verbose", false);
-
     auto chunks = GetChunks(payload, chunkSize);
     std::string debugPayload;
 
@@ -21,9 +19,6 @@ void SetXpBarServerScript::SendChunkedPayload(Warden* warden, WardenPayloadMgr* 
 
     payloadMgr->QueuePayload(_prePayloadId);
     warden->ForceChecks();
-
-    if (verbose)
-        LOG_INFO("module", "SetXpBar: Sent pre-payload '%s'.", _prePayload.c_str());
 
     for (auto const& chunk : chunks)
     {
@@ -38,9 +33,6 @@ void SetXpBarServerScript::SendChunkedPayload(Warden* warden, WardenPayloadMgr* 
         payloadMgr->RegisterPayload(smallPayload, _tmpPayloadId, true);
         payloadMgr->QueuePayload(_tmpPayloadId);
         warden->ForceChecks();
-
-        if (verbose)
-            LOG_INFO("module", "SetXpBar: Sent mid-payload '%s'.", smallPayload.c_str());
     }
 
     if (!payloadMgr->GetPayloadById(_postPayloadId))
@@ -48,33 +40,24 @@ void SetXpBarServerScript::SendChunkedPayload(Warden* warden, WardenPayloadMgr* 
 
     payloadMgr->QueuePayload(_postPayloadId);
     warden->ForceChecks();
-
-    if (verbose)
-    {
-        LOG_INFO("module", "SetXpBar: Sent post-payload '%s'.", _postPayload.c_str());
-        LOG_INFO("module", "SetXpBar: Reassembled payload: '%s'.", debugPayload.c_str());
-    }
 }
 
 bool SetXpBarServerScript::CanPacketSend(WorldSession* session, WorldPacket& packet)
 {
     if (!sConfigMgr->GetOption<bool>("SetXpBar.Enable", false))
     {
-        LOG_INFO("module", "SetXpBar: Module disabled in configuration (opcode %u)", packet.GetOpcode());
         return true;
     }
 
     WardenWin* warden = (WardenWin*)session->GetWarden();
     if (!warden || !warden->IsInitialized())
     {
-        LOG_INFO("module", "SetXpBar: Warden not initialized (opcode %u, initialized: %s)", packet.GetOpcode(), warden ? (warden->IsInitialized() ? "true" : "false") : "null");
         return true;
     }
 
     auto payloadMgr = warden->GetPayloadMgr();
     if (!payloadMgr)
     {
-        LOG_INFO("module", "SetXpBar: PayloadMgr not available (opcode %u)", packet.GetOpcode());
         return true;
     }
 
@@ -86,7 +69,6 @@ bool SetXpBarServerScript::CanPacketSend(WorldSession* session, WorldPacket& pac
         payloadMgr->UnregisterPayload(_tmpPayloadId);
 
         SendChunkedPayload(warden, payloadMgr, _midPayload, 64);
-        LOG_INFO("module", "SetXpBar: SetXpBar.lua payload queued for in-game (%u bytes)", _midPayload.size());
     }
 
     return true;
